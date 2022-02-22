822583677bae63af84844c6df22397095c50c003
JunyiZhao815 <92706300+JunyiZhao815@users.noreply.github.com>
2022-02-22 11:22:22 +0800

    Add files via upload

---
+++ Inclass_Feb22.py
@@ -0,0 +1,11 @@
+
+
+name = str(input("Please input your name: "))
+age = int(input("Please input your age: "))
+address = str(input("Please input your address: "))
+favorite_sport = str(input("Please input your favorite sport: "))
+
+print("Hello, my name is:", name)
+print("I am",age,"years")
+print("I live in",address)
+print("I like to play",favorite_sport)
